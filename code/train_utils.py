a8835c5a3d658d1154a303bbef1a88415afb6fe5
Amanda Olmin <amaol67@liu.se>
2023-12-19 16:40:26 +0100

    Now clean-up is actually working

--- code/train_utils.py
+++ code/train_utils.py
@@ -1,6 +1,7 @@
 import pathlib
 import shutil
 import json
+import math
 import numpy as np
 from PIL import Image
 
@@ -226,26 +227,26 @@ def clear_gradients(model):
             
 # Initialisation
 
-def kaiming_init(model, args):
+def kaiming_init(model, g_cpu, args):
 
     i = 0
     with torch.no_grad():
         for m in model:
             if type(m) == torch.nn.Linear:
                 if i < (args.num_layers - 1): # NOTE: hade av misstag lika med här innan (i==0), så fem-lager-resultaten hade inte riktigt denna initialisering
-                    torch.nn.init.kaiming_normal_(m.weight, a=math.sqrt(5), generator=g_cpu)
+                    torch.nn.init.kaiming_normal_(m.weight, a=math.sqrt(5)) #, generator=g_cpu)
                     m.weight.data = torch.mul(m.weight.data, args.scales[0])
                     print(m.weight.data.shape, args.scales[0])
                     
                 elif i == (args.num_layers - 1): 
-                    torch.nn.init.kaiming_uniform_(m.weight, a=math.sqrt(5), generator=g_cpu)
+                    torch.nn.init.kaiming_uniform_(m.weight, a=math.sqrt(5)) #, generator=g_cpu)
                     m.weight.data = torch.mul(m.weight.data, args.scales[1])
                     print(m.weight.data.shape, args.scales[1])
                 i += 1
             elif type(m) == ScalingLayer:
-                torch.nn.init.kaiming_normal_(m.inner_weight, a=math.sqrt(5), generator=g_cpu)
+                torch.nn.init.kaiming_normal_(m.inner_weight, a=math.sqrt(5)) #, generator=g_cpu)
                 m.inner_weight.data = torch.mul(m.inner_weight.data, args.scales[0])
-                torch.nn.init.kaiming_uniform_(m.weight, a=math.sqrt(5), generator=g_cpu)
+                torch.nn.init.kaiming_uniform_(m.weight, a=math.sqrt(5)) #, generator=g_cpu)
                 m.weight.data = torch.mul(m.weight.data, args.scales[1])
 
     return model
@@ -276,29 +277,33 @@ def rank_one_init(model, g_cpu, args):
 
     i = 0
     with torch.no_grad(): 
-        p, q, u = 0, 0
+        p, q, u = 0, 0, 0
         for m in model:
             if type(m) == torch.nn.Linear:
                                 
                 if i == 0:
-                    q = torch.normal(mean=0, std=scales[0], size=(m.weight.data.shape[0], 1), generator=g_cpu)
-                    p = torch.nn.normal(mean=0, std=1, size=(m.weight.data.shape[1], 1), generator=g_cpu)
+                    q = torch.normal(mean=0, std=args.scales[0], size=(m.weight.data.shape[1], 1), generator=g_cpu)
                     u = 1
                 else:
                     q = p.clone()
-                    p = torch.normal(mean=0, std=1, (m.weight.data.shape[1], 1), generator=g_cpu)
                     
                     if i == 1:
-                        u = torch.normal(mean=0, std=scales[1], size=(), generator=g_cpu)
+                        u = torch.normal(mean=0, std=args.scales[1], size=(), generator=g_cpu)
                     
+                p = torch.normal(mean=0, std=1, size=(m.weight.data.shape[0], 1), generator=g_cpu)
                 p /= torch.norm(p, dim=0)
 
                 m.weight.data = u * torch.matmul(p, q.T)
-                
+
                 i += 1
                 
             elif type(m) == ScalingLayer:
-                m.inner_weight.data = torch.ones(m.inner_weight.data.shape) * args.scales[0]
-                m.weight.data = torch.ones(m.weight.data.shape) * args.scales[1]
+                z = torch.normal(mean=0, std=args.scales[0], size=(m.inner_weight.data.shape[1], 1), generator=g_cpu)
+                p = torch.normal(mean=0, std=1, size=(m.inner_weight.data.shape[0], 1), generator=g_cpu)
+                q = torch.normal(mean=0, std=1, size=(m.weight.data.shape[0], 1), generator=g_cpu)
+                u = torch.normal(mean=0, std=args.scales[1], size=(), generator=g_cpu)
+                
+                m.inner_weight.data = torch.matmul(p, z.T)
+                m.weight.data = u * torch.matmul(q, p.T)
                 
     return model 
--- two_layer.py
+++ two_layer.py
@@ -16,87 +16,13 @@ import torch
 import sys
 sys.path.append('code/')
 from linear_utils import linear_model, is_float
-from train_utils import save_config, prune_data, calculate_weight_mse, extract_weights, ScalingLayer, kaiming_normal_init, fixed_init, rank_one_init
+from train_utils import save_config, prune_data, calculate_weight_mse, extract_weights, ScalingLayer, kaiming_init, fixed_init, rank_one_init
 from theoretical_model import linear_two_layer_simulation
 
 from sharpness_utilities import sharpness
 
 import wandb
 
-
-def get_args():
-    parser = argparse.ArgumentParser(description='CLI parameters for training')
-    parser.add_argument('--config', type=str, default='', metavar='CONFIG',
-                        help='Config file')
-    parser.add_argument('--root', type=str, default='', metavar='DIR',
-                        help='Root directory')
-    parser.add_argument('-t', '--iterations', type=int, default=1e4, metavar='ITERATIONS',
-                        help='Iterations (default: 1e4)')
-    parser.add_argument('-n', '--samples', type=int, default=100, metavar='N',
-                        help='Number of samples (default: 100)')
-    parser.add_argument('--print-freq', type=int, default=100,
-                        help='CLI output printing frequency (default: 1000)')
-    parser.add_argument('--gpu', type=int, default=None,
-                        help='Number of GPUS to use')
-    parser.add_argument('--disable-cuda', action='store_true', default=False,
-                        help='Disable CUDA')
-    parser.add_argument('--seed', type=int, default=None,
-                        help='Random seed')
-    parser.add_argument('-d', '--dim', type=int, default=50, metavar='DIMENSION',
-                        help='Feature dimension (default: 50)')
-    parser.add_argument('--hidden', type=int, default=200, metavar='DIMENSION',
-                        help='Hidden layer dimension (default: 200)')
-    parser.add_argument('--batch-norm', action='store_true', default=False,
-                        help='Use batch norm')
-    parser.add_argument('--no-bias', action='store_true', default=False,
-                        help='Do not use bias')
-    parser.add_argument('--linear', action='store_true', default=False,
-                        help='Linear activation function')
-    parser.add_argument('--sigmas', type=str, default=None,
-                        help='Sigmas')
-    parser.add_argument('-r', '--s-range', nargs='*', type=float,
-                        help='Range for sigmas')
-    parser.add_argument('-w', '--scales', nargs='*', type=float,
-                        help='scale of the weights')
-    parser.add_argument('--lr', type=float, default=1e-4, nargs='*', metavar='LR',
-                        help='learning rate (default: 1e-4)')
-    parser.add_argument('--normalized', action='store_true', default=False,
-                        help='normalize sample norm across features')
-    parser.add_argument('--risk-loss', type=str, default='MSE', metavar='LOSS',
-                        help='Loss for validation')
-    parser.add_argument('--jacobian', action='store_true', default=False,
-                        help='compute the SVD of the jacobian of the network')
-    parser.add_argument('--save-results', action='store_true', default=False,
-                        help='Save the results for plots')
-    parser.add_argument('--plot', action='store_true', default=False,
-                        help='Plot the results')
-    parser.add_argument('--eigen', action='store_true', default=False,
-                        help='Compute eigenvalue')
-    parser.add_argument('--pcs', default=None,
-                        help='Number of PCs to use in data.')
-    parser.add_argument('--details', type=str, metavar='N',
-                        default='no_detail_given',
-                        help='details about the experimental setup')
-
-    args = parser.parse_args(sys.argv[1:])
-
-    # directories
-    root = pathlib.Path(args.root) if args.root else pathlib.Path.cwd().parent
-
-    current_date = str(datetime.datetime.today().strftime('%Y-%m-%d-%H-%M-%S'))
-    args.outpath = (pathlib.Path.cwd().parent / 'results' / 'two_layer_nn' / current_date)
-
-    if args.save_results:
-        args.outpath.mkdir(exist_ok=True, parents=True)
-
-    if args.seed is not None:
-        random.seed(args.seed)
-        torch.manual_seed(args.seed)
-        np.random.seed(args.seed)
-
-    args.device = 'cuda' if (not args.disable_cuda and torch.cuda.is_available()) else 'cpu'
-
-    return args
 
 def train_model(model, Xs, ys, Xt, yt, Xs_low, true_weights, stepsize, args):
     # define loss functions
@@ -285,7 +211,7 @@ def init_model_params(model, args):
 
         if args.fixed_weight_init:
             model = fixed_init(model, args)
-        elif args.rank_one_init:
+        elif args.one_rank_init:
             model = rank_one_init(model, g_cpu, args)
         else:
             model = kaiming_init(model, g_cpu, args)
@@ -448,12 +374,12 @@ def append_id(filename, id):
 
 def get_run_name(args):
 
-    key_word_dict = {"lr1": args.lr[0], "lr2": args.lr[1], "kappa": args.kappa, "hidden": args.hidden, "")
-                    
-    if args.key_word in dict:
-        print("Unrecognised key word")
-        run_name = args.key_word + f"_{key_word_dict[args.key_word]}
+    key_word_dict = {"lr1": args.lr[0], "lr2": args.lr[1], "kappa": args.kappa, "hidden": args.hidden, "": ""}
+            
+    if args.key_word in key_word_dict:
+        run_name = args.key_word + f"_{key_word_dict[args.key_word]}"
     else:
+        print("Unrecognised key word")
         run_name = args.key_word
         
         assert args.key_word != "", "Empty run name"
@@ -501,19 +427,20 @@ def save_results(args, res):
     result_path = get_result_path(args)
     run_name = get_run_name(args)
     
-    result_file = os.path.join(base_dir, run_name + ".txt")
+    result_file = os.path.join(result_path, run_name + ".txt")
+    print("Saving data to " + result_file)
     
     unravelled_res = {}
     for key, value in res.items():
         if value.ndim > 1:
             for i in range(value.shape[-1]):
-                unravelled_res[key + "_" + str(i + 1)] = value[:, i]
+                unravelled_res[key + "_" + str(i + 1)] = value[:, i].tolist()
             
         else:
-            unravelled_res[key] = value
+            unravelled_res[key] = value.tolist()
     
     with open(result_file, "w") as f:
-        json.dump(res, f)
+        json.dump(unravelled_res, f)
         
 
 def plot_results_from_file(result_path, args):
@@ -554,9 +481,7 @@ def main(args):
 
     #additional_data = [out[key] for key in out if (out[key].size != 0 and key not in ["risk", "loss", "weight_norm", "grad_norm"])] 
     #save_results(args, out["risk"], out["loss"], additional_data)
-    for key in out:
-        if out[key].size != 0 and key not in ["weight_norm", "grad_norm"]:
-            save_results
+    save_results(args, out)
     
     if args.plot:  # for debugging
         plot_results_from_file(get_result_path(args), args)
